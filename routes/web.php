aef065fdca1f4846d00ce65a7faeb15c22b73e9f
Jagraj <jagraj3101@gmail.com>
2025-12-12 02:23:17 +0530

    Corrected static resources and extracted feeds

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,80 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/feed', function () {
-    return view('feed');
+Route::get('/feed', function() {
+
+    $feedItems = json_decode(json_encode([
+        [
+            'postedDateTime' => '3h',
+            'content' => <<<str
+                <p>
+                    I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                </p>
+                <img src="/images/simon-chilling.png" alt="" />
+            str,
+            'likeCount' => 24,
+            'replyCount' => 33,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/images/michael.png',
+                'displayName' => 'Michael',
+                'handle' => '@mmich_jj',
+            ],
+            'replies' => [
+                [
+                    'content' => '<p>Heh — this looks just like me!</p>',
+                    'postedDateTime' => '1h',
+                    'likeCount' => 72,
+                    'replyCount' => 11,
+                    'repostCount' => 300,
+                    'profile' => [
+                        'avatar' => '/images/simon-chilling.png',
+                        'displayName' => 'Simon',
+                        'handle' => '@simonswiss',
+                    ],
+                ]
+            ]
+        ]
+    ]));
+
+
+    return view('feed', compact('feedItems'));
 });
 
-Route::get('/profile', function () {
-    return view('profile');
+Route::get('/profile', function() {
+    $feedItems = json_decode(json_encode([
+        [
+            'postedDateTime' => '3h',
+            'content' => <<<str
+                <p>
+                    I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                </p>
+                <img src="/images/simon-chilling.png" alt="" />
+            str,
+            'likeCount' => 24,
+            'replyCount' => 33,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/images/michael.png',
+                'displayName' => 'Michael',
+                'handle' => '@mmich_jj',
+            ],
+            'replies' => [
+                [
+                    'content' => '<p>Heh — this looks just like me!</p>',
+                    'postedDateTime' => '1h',
+                    'likeCount' => 72,
+                    'replyCount' => 11,
+                    'repostCount' => 300,
+                    'profile' => [
+                        'avatar' => '/images/simon-chilling.png',
+                        'displayName' => 'Simon',
+                        'handle' => '@simonswiss',
+                    ],
+                ]
+            ]
+        ]
+    ]));
+
+    return view('profile', compact('feedItems'));
 });
