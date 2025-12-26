704d9dd31e691831974723db6bb79dc4926f2b89
Jagraj <jagraj3101@gmail.com>
2025-12-27 01:17:19 +0530

    Generated Post/Comments/Reply model

---
+++ app/Models/Post.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Profile;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'profile_id',
+        'parent_id',
+        'content',
+    ];
+
+    public function profile(): BelongsTo
+    {
+        return $this->belongsTo(Profile::class);
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'parent_id');
+    }
+
+    public function replies(): HasMany
+    {
+        return $this->hasMany(Post::class, 'parent_id');
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Profile;
+use App\Models\Post;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'profile_id' => Profile::factory(),
+            'parent_id' => null,
+            'content' => $this->faker->realText(200),
+        ];
+    }
+
+    public function reply(Post $parentPost) {
+
+        return $this->state([
+            'parent_id' => $parentPost->id,
+            'content' => $this->faker->realText(200),
+        ]);
+    }
+}
